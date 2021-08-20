6c9e317dc215b2b6b8b62cb1e0e75de8f266df34
nicky0830 <75279575+nicky0830@users.noreply.github.com>
2021-08-21 03:30:44 +0900

    src\components\Main

--- style.jsx
+++ style.jsx
@@ -1,22 +1,217 @@
 import styled from "styled-components";
 
-export const NavBarContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 90%; 
-  margin: auto;
-  background: #FFFFFF;
+
+
+export const SearchContainer = styled.div`
+display: flexbox;
+width: 60%; 
+margin: auto;
+padding-top: 20px;
+background: #FFFFFF;
+`
+
+export const SearchBar = styled.input`
+  padding: 0.5rem 3rem;
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+  color: black;
+
+  display: inline-block;
+  width: auto;
+  background-color: #F5F5F5;
+
+  border: none;
+  border-radius: 20px;
+  box-shadow: 5px 5px rgba(191, 191, 191, 0.4);
+  flex:1;
+`;
+export const SearchBtn = styled.div`
+flex:1;
+margin-left: 20px;
+display: block; 
+width: 30px; 
+height: 30px; 
+
+padding: 0.2rem 2rem;
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+  color: black;
+
+  display: inline-block;
+  width: auto;
+  background-color: #c32020;
+
+  border: none;
+  border-radius: 20px;
+  box-shadow: 5px 5px rgba(191, 191, 191, 0.4);
 
 `;
-export const NavBarLogoContainer = styled.div`
- width: 80%;
- margin: auto;
- background-color: red;
-`;
-export const NavBarAuth = styled.div`
-`;
-export const NavBarMenuContainer = styled.div`
-  display: flex;
-  background: #FFFFFF;
+export const MainTopWrapper = styled.div`
+width: 60%;
+margin: auto;
+background: #FFFFFF;
 
 `;
+
+export const MainTopContainer = styled.div`
+background-color: #FFFFFF;
+`
+export const MainTitle = styled.div`
+  & > span{ 
+    background: #FFFFFF;
+  }
+
+font-size: 80px;
+margin: 100px 0px 10px 0px;
+background: #FFFFFF;
+`;
+
+
+
+export const MainExplain = styled.div`
+font-size: 20px;
+background: #FFFFFF;
+
+& > span{ 
+  background: #FFFFFF;
+}`;
+
+export const SessionContainer = styled.div`
+`;
+export const SessionImg = styled.div``;
+export const SessionExplainWrapper = styled.div`
+position: relative;
+
+`;
+export const SessionEachWrapper = styled.div`
+background-color: #FFFFFF;
+display: flexbox; 
+flex-direction: row;
+justify-content: space-between;
+padding: 20px 10px;
+margin-bottom: 20px;
+border-radius: 20px;
+box-shadow: 5px 5px rgba(191, 191, 191, 0.2);
+
+
+`
+
+export const SessionText = styled.div`
+margin-left: 30px;
+background-color:#FFFFF;
+
+`;
+export const SessionTitle = styled.div`
+& > p { 
+  background-color:#FFFFF;
+}
+font-weight: bold;
+font-size: 25px;
+margin-bottom: 5px;
+
+`;
+export const SessionExplain = styled.div`
+font-size: 20px;
+& > p { 
+  background-color:#FFFFF;
+}
+
+`;
+export const SessionFavorite = styled.div`
+padding: 0.8rem 2rem;
+
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+
+  display: inline-block;
+  width: auto;
+  color: white; 
+  background-color: #c32020;
+
+  border: none;
+  border-radius: 20px;
+
+
+margin-right: 30px;
+`;
+
+export const MainBottomWrapper = styled.div`
+width: 60%; 
+margin: auto;
+position: relative;
+`
+export const MainBottomBtnCont = styled.div`
+display: flexbox;
+flex-direction: row;
+margin: 50px 0px;
+`;
+export const MainSessDuration = styled.div`
+& > p{ 
+  background-color: #FFFFFF;
+}
+padding: 0.8rem 3rem;
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+  color: black;
+
+  display: inline-block;
+  width: auto;
+  background-color: #c32020;
+  
+
+  border: none;
+  border-radius: 20px;
+
+
+`;
+export const MainSessItem = styled.div`
+margin-left: 5px;
+padding: 0.8rem 3rem;
+
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+
+  display: inline-block;
+  width: auto;
+
+  border: none;
+  border-radius: 20px;
+  font-weight: bold;
+
+`;
+export const MainSessTab = styled.div`
+ display: flexbox; 
+ flex-direction: row;
+`;
+export const MainSessRig = styled.div`
+position: absolute;
+right: 0px;
+padding: 0.8rem 3rem;
+
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+
+  display: inline-block;
+  width: auto;
+  background-color:#FFFFFF;
+  box-shadow: 5px 5px rgba(191, 191, 191, 0.4);
+ 
+
+  border: none;
+  border-radius: 30px;
+  font-weight: bold;
+`;
+export const MainSessItemsWrapper = styled.div``;
+
