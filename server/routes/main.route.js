b594418b0b9aad5dd9059b8bec2cbd08cf4c0756
cherry-in <sag06078@naver.com>
2021-08-21 06:03:05 +0900

    ㅁ

--- server/routes/main.route.js
+++ server/routes/main.route.js
@@ -3,6 +3,8 @@ import mainCtrl from "../controllers/main.controller.js";
 
 const router = express.Router();
 
-router.route("/").get(mainCtrl.test);
+router.route("/").get(mainCtrl.findSessions,mainCtrl.findStudies,mainCtrl.sendClasses);
+router.route("/sessions").get(mainCtrl.findSessions,mainCtrl.sendClasses);
+router.route("/studies").get(mainCtrl.findStudies,mainCtrl.sendClasses);
 
 export default router;
